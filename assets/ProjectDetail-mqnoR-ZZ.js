b8e3e9fa1fb4a4365116cb7ff6f4092fa76bbf4a
Rachel-Lev <rachel.lev@dragontailsystems.com>
2026-05-08 00:54:17 +0300

    Updates

--- assets/ProjectDetail-mqnoR-ZZ.js
+++ assets/ProjectDetail-mqnoR-ZZ.js
@@ -1,4 +1,4 @@
-import{c as v,u as C,r as l,j as e,A as z,m as w,X as S,a as I,L as k}from"./index-DLZAQO-5.js";import{g as F,M as W}from"./MasonryGrid-RR3gMaHv.js";import"./ScrollFadeIn-vFPWmRGM.js";/**
+import{c as v,u as C,r as l,j as e,A as z,m as w,X as S,a as I,L as k}from"./index-Cr9ZVXVf.js";import{g as F,M as W}from"./MasonryGrid-BniU-8YJ.js";import"./ScrollFadeIn-DE2LyNMh.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
